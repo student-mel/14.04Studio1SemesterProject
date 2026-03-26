14df191f83ed07192f0b9703ab9ba6dbdace9e35
LearnerShen <teojunshen001001@gmail.com>
2026-03-26 13:02:37 +1100

    Last move UI tweaks
    Added runtime update to scaling, fading, and change length through inspector. Fixed execution order issue.

--- Assets/Scripts/Inputs/MoveUI.cs
+++ Assets/Scripts/Inputs/MoveUI.cs
@@ -1,91 +1,232 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 public class MoveUI : MonoBehaviour
 {
-    public int Index;
-    public int Length;
+    [Header("Player Setting")]
+
+    [Range(1, 2)] public int Index;
+
+    [Header("UI Settings")]
+
+    [Range(1, 10)] public int Length;
+    private int currLength;
+
+    public event Action OnLengthChanged;
+
+    public int LengthValue
+    {
+        get => currLength;
+        set
+        {
+            if (currLength == value) return;
+            int oldValue = currLength;
+            currLength = value;
+            OnLengthChanged?.Invoke();
+        }
+    }
+
     [Range(0.5f, 1.0f)] public float Scale;
+    private float currScale;
+
+    public event Action OnScaleChanged;
+
+    public float ScaleValue
+    {
+        get => currScale;
+        set
+        {
+            if (currScale == value) return;
+            float oldValue = currScale;
+            currScale = value;
+            OnScaleChanged?.Invoke();
+        }
+    }
+
+    [Range(0.0f, 1.0f)] public float Fade;
+    private float currFade;
+
+    public event Action OnFadeChanged;
+
+    public float FadeValue
+    {
+        get => currFade;
+        set
+        {
+            if (currFade == value) return;
+            float oldValue = currFade;
+            currFade = value;
+            OnFadeChanged?.Invoke();
+        }
+    }
+
+    [Header("Spawn Settings")]
+
     public Vector3 StartPosition;
     public Vector2 AnchorPosition;
+
+    [Tooltip("Do not touch unless you know what you are doing")]
+    [Header("Reference Settings")]
 
     public string[] spriteNames;
 
     public GameObject textPrefab;
 
-    private List<TMP_Text> queue = new List<TMP_Text>();
+    private List<TMP_Text> textList = new List<TMP_Text>();
+    private List<int> textStack = new List<int>();
+
     private RectTransform rectTransform;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+
+        currLength = Length;
+        for (int i = 0; i < currLength; i++)
+        {
+            GameObject textObj = Instantiate(textPrefab, transform);
+
+            textList.Add(textObj.GetComponent<TMP_Text>());
+            textList[i].text = "";
+
+            textStack.Add(0);
+
+            UpdateTextPosition();
+        }
+    }
+
+    private void OnEnable()
+    {
+        OnLengthChanged += UpdateTextList;
+        OnScaleChanged += UpdateTextPosition;
+        OnFadeChanged += UpdateTextPosition;
+    }
+    private void OnDisable()
+    {
+        OnLengthChanged -= UpdateTextList;
+        OnScaleChanged -= UpdateTextPosition;
+        OnFadeChanged -= UpdateTextPosition;
+    }
+
+    private void Update()
+    {
+        LengthValue = Length;
+        ScaleValue = Scale;
+        FadeValue = Fade;
     }
 
     public void AddMoveToQueue(int? attackSpriteIndex, int? dirSpriteIndex)
     {
-        GameObject textObj = Instantiate(textPrefab, transform);
-        textObj.transform.position = StartPosition;
-        TMP_Text text = textObj.GetComponent<TMP_Text>();
+        string newText = "";
 
-        text.text = "";
         if (dirSpriteIndex != null)
         {
-            text.text += $"<sprite name={spriteNames[(int)dirSpriteIndex]}>";
+            newText += $"<sprite name={spriteNames[(int)dirSpriteIndex]}>";
         }
         if(attackSpriteIndex != null)
         {
-            if (text.text != "")
-                text.text += "+";
-            text.text += $"<sprite name={spriteNames[(int)attackSpriteIndex]}>";
+            if (newText != "")
+                newText += "+";
+            newText += $"<sprite name={spriteNames[(int)attackSpriteIndex]}>";
         }
 
-        if(text.text != "")
-            queue.Insert(0, text);
-        if(queue.Count > Length)
+        if(newText != "")
         {
-            Destroy(queue[queue.Count - 1].gameObject);
-            queue.RemoveAt(queue.Count - 1);
+            string lastMoveText = "";
+
+            if (Index == 1)
+                lastMoveText = textList[0].text.Replace($" {textStack[0]}", "");
+            else if (Index == 2)
+                lastMoveText = textList[0].text.Replace($"{textStack[0]} ", "");
+
+            if (!lastMoveText.Equals(newText))
+            {
+                for (int i = textList.Count - 1; i >= 1; i--)
+                {
+                    textList[i].text = textList[i - 1].text;
+                    textStack[i] = textStack[i - 1];
+                }
+                textList[0].text = newText;
+                textStack[0] = 1;
+            }
+            else
+            {
+                textStack[0]++;
+                if (Index == 1)
+                    textList[0].text = newText + $" {textStack[0]}";
+                else if (Index == 2)
+                    textList[0].text = $"{textStack[0]} " + newText;
+            }
         }
-        UpdateTextPosition();
     }
     private void UpdateTextPosition()
     {
-        for(int i = 0; i < queue.Count; i++)
+        for(int i = 0; i < textList.Count; i++)
         {
             Vector3 newPos = new Vector3();
-            float newHeight;
-            float newWidth;
 
             int lastIndex = (i - 1) >= 0 ? i - 1 : i;
-
-            newHeight = queue[lastIndex].rectTransform.rect.height * Scale;
-            newWidth = queue[lastIndex].rectTransform.rect.width * Scale;
 
             if (Index == 1)
             {
                 newPos =    StartPosition                                   + 
                             (Vector3.up * rectTransform.rect.height / 2)    + 
-                            Vector3.down * i * queue[i].rectTransform.rect.height;
+                            Vector3.down * i * textList[0].fontSize;
 
-                queue[i].alignment = TextAlignmentOptions.MidlineLeft;
+                textList[i].alignment = TextAlignmentOptions.MidlineLeft;
             }
             else if (Index == 2)
             {
                 newPos =    (Vector3.right * rectTransform.rect.width)      +
                             StartPosition                                   + 
                             (Vector3.up * rectTransform.rect.height / 2)    + 
-                            Vector3.down * i * queue[i].rectTransform.rect.height;
+                            Vector3.down * i * textList[0].fontSize;
 
-                queue[i].alignment = TextAlignmentOptions.MidlineRight;
+                textList[i].alignment = TextAlignmentOptions.MidlineRight;
             }
 
-            Debug.Log(newPos);
-            queue[i].rectTransform.anchorMin = AnchorPosition;
-            queue[i].rectTransform.anchorMax = AnchorPosition;
-            queue[i].rectTransform.position = newPos;
-            queue[i].rectTransform.rect.Set(newPos.x, newPos.y, newWidth, newHeight);
+            textList[i].rectTransform.anchorMin = AnchorPosition;
+            textList[i].rectTransform.anchorMax = AnchorPosition;
+            textList[i].rectTransform.position = newPos;
+            if(i > 0)
+                textList[i].fontSize = textList[lastIndex].fontSize * Scale ;
+
+            Color newColor = textList[i].color;
+            newColor.a = Mathf.Pow(Fade, i);
+            textList[i].color = newColor;
+        }
+    }
+    private void UpdateTextList()
+    {
+        if(LengthValue > textList.Count)
+        {
+            int diff = LengthValue - textList.Count;
+            for (int i = 0; i < diff; i++)
+            {
+                GameObject textObj = Instantiate(textPrefab, transform);
+
+                TMP_Text newText = textObj.GetComponent<TMP_Text>();
+                newText.text = "";
+                textList.Add(newText);
+
+                textStack.Add(0);
+            }
+            UpdateTextPosition();
+        }
+        else if(LengthValue < textList.Count)
+        {
+            int diff = textList.Count - LengthValue;
+            for (int i = 0; i < diff; i++)
+            {
+                TMP_Text textObj = textList[textList.Count - 1 - i];
+
+                textList.Remove(textObj);
+
+                Destroy(textObj.gameObject);
+            }
         }
     }
 }
@@ -105,7 +246,7 @@ public class MoveUIEditor : Editor
 
         if(GUILayout.Button("Add Move"))
         {
-            moveUI.AddMoveToQueue(Random.Range(0, 2), 2);
+            moveUI.AddMoveToQueue(UnityEngine.Random.Range(0, 2), 2);
         }
 
         EditorGUI.EndDisabledGroup();
--- Assets/Scripts/Inputs/PlayerInputHandler.cs
+++ Assets/Scripts/Inputs/PlayerInputHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -11,6 +12,11 @@ public class PlayerInputHandler : MonoBehaviour
     public UnityAction AttackEndedEvent;
 
     private PlayerInput input;
+    private Vector2 moveInput = new Vector2();
+
+    private bool attackedThisFrame = false;
+
+    private MoveUI moveDisplay;
 
     public bool debug = false;
 
@@ -20,6 +26,9 @@ public class PlayerInputHandler : MonoBehaviour
     {
         input = GetComponent<PlayerInput>();
         PlayerIndex = input.user.index + 1;
+
+        MoveUI[] moveUIs = FindObjectsByType<MoveUI>(FindObjectsSortMode.None);
+        moveDisplay = moveUIs.FirstOrDefault(m => m.Index == PlayerIndex);
     }
 
     public void OnAttack(InputAction.CallbackContext context)
@@ -28,21 +37,51 @@ public class PlayerInputHandler : MonoBehaviour
         {
             AttackEvent?.Invoke();
 
+            attackedThisFrame = true;
+
             if (debug)
                 Debug.Log($"Player {(input.user.index + 1)} Attacked");
         }
 
-
-        if(context.canceled) AttackEndedEvent?.Invoke();
+        if (context.canceled) AttackEndedEvent?.Invoke();
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+        moveInput = context.ReadValue<Vector2>();
+
+        if(Mathf.Abs(moveInput.x) > 0.1f)
+            MoveEvent?.Invoke(moveInput);
 
         if(debug)
             Debug.Log($"Player {(input.user.index + 1)} Moving");
 
         if(context.canceled) MoveEndedEvent?.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        DisplayMove();
+    }
+
+    public void DisplayMove()
+    {
+        if (attackedThisFrame)
+        {
+            if (moveInput.x > 0.1f)
+                moveDisplay.AddMoveToQueue(2, 1);
+            else if (moveInput.x < -0.1f)
+                moveDisplay.AddMoveToQueue(2, 0);
+            else
+                moveDisplay.AddMoveToQueue(2, null);
+            attackedThisFrame = false;
+        }
+        else
+        {
+            if (moveInput.x > 0.1f)
+                moveDisplay.AddMoveToQueue(null, 1);
+            else if (moveInput.x < -0.1f)
+                moveDisplay.AddMoveToQueue(null, 0);
+        }
     }
 }
