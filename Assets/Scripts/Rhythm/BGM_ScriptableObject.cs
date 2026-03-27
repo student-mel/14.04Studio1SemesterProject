e8ee93cfbadb52426493bda1de6388b60742915a
student-mel <melonym1101@gmail.com>
2026-03-27 21:24:01 +1100

    Rhythm Framework scripts
    new scripts for an independent, single scope framework.

---
+++ Assets/Scripts/Rhythm/BGM_ScriptableObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjBGM_", menuName = "BGM Scriptable Object", order = 0)]
+public class BGM_Rhythm : ScriptableObject
+{
+    public AudioClip clip;
+    [Range(80, 180)]
+    public int bpm; 
+}
