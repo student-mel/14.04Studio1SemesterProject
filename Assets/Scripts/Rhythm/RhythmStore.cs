5675d3212354716e809e247d365aa1e9293f13e5
student-mel <melonym1101@gmail.com>
2026-03-27 21:26:18 +1100

    Added EventBus and RhythmStore
    Simple new EventBus for custom events and RhythmStore to communicate with Rhythm framework

---
+++ Assets/Scripts/Rhythm/RhythmStore.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class RhythmStore : MonoBehaviour
+{
+    public static RhythmStore Instance;
+
+    [Header("Music")]
+    public float musicTimeMs;
+    public bool isPlaying;
+
+    [Header("Metronome")]
+    public int currentBeatIndex;
+    public float currentBeatTimeMs;
+
+    [Header("Judge")]
+    public float lastTimingOffsetMs;
+    public string lastJudgement;
+
+    /*[Header("Combat")]
+    public float lastDamage;*/
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void SetMusicTime(float timeMs)
+    {
+        musicTimeMs = timeMs;
+    }
+
+    public void SetBeat(int beatIndex, float beatTime)
+    {
+        currentBeatIndex = beatIndex;
+        currentBeatTimeMs = beatTime;
+    }
+
+    public void SetJudgement(string judgement, float offset)
+    {
+        lastJudgement = judgement;
+        lastTimingOffsetMs = offset;
+    }
+
+    /*public void SetDamage(float dmg)
+    {
+        lastDamage = dmg;
+    }*/
+}
