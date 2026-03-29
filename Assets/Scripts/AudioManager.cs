3173a1ecdd6e75869d44cb85fb2caa6faa92509f
Pac-ManLikeGameCreator <amitesh.d.amiteshsharma@student.uts.edu.au>
2026-03-29 11:59:34 +1100

    Update AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,35 +1,73 @@
-using UnityEngine; //
+using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
 
+    [Header("Audio Sources")]
     public AudioSource sfxSource;
     public AudioSource voiceSource;
 
     [Header("SFX")]
     public AudioClip hit;
     public AudioClip ko;
+    // public AudioClip block;   
+    // public AudioClip whiff;   
 
     [Header("Announcer")]
+    public AudioClip preRoundVoice;
     public AudioClip roundStartVoice;
     public AudioClip koVoice;
 
     void Awake()
     {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    public void PlayHit() => sfxSource.PlayOneShot(hit);
+    public void PlayHit()
+    {
+        if (hit != null && sfxSource != null)
+            sfxSource.PlayOneShot(hit);
+    }
 
     public void PlayKO()
     {
-        sfxSource.PlayOneShot(ko);
-        voiceSource.PlayOneShot(koVoice);
+        if (ko != null && sfxSource != null)
+            sfxSource.PlayOneShot(ko);
+
+        if (koVoice != null && voiceSource != null)
+            voiceSource.PlayOneShot(koVoice);
+    }
+
+    /*sfx for block and whiff 
+    public void PlayBlock()
+    {
+      if (block != null && sfxSource != null)
+            sfxSource.PlayOneShot(block);
+    }
+
+    public void PlayWhiff()
+    {
+         if (whiff != null && sfxSource != null)
+             sfxSource.PlayOneShot(whiff);
+    }*/
+
+    public void PlayPreRound()
+    {
+        if (preRoundVoice != null && voiceSource != null)
+            voiceSource.PlayOneShot(preRoundVoice);
     }
 
     public void PlayRoundStart()
     {
-        voiceSource.PlayOneShot(roundStartVoice);
+        if (roundStartVoice != null && voiceSource != null)
+            voiceSource.PlayOneShot(roundStartVoice);
     }
 }
